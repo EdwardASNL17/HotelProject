19a6956fdce88dbbbaaee7b0f8f0944dc14c0978
Didenko17 <vladislav.didenko.2001@gmail.com>
2020-12-23 21:02:44 +0300

    Добавлено pop-up окно

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,6 +1,8 @@
+import 'moment/locale/ru';
+import moment from 'moment';
+import locale from 'antd/es/date-picker/locale/ru_RU';
 import React, { useState, useEffect } from 'react';
 import { Form, Input, Button, Typography, DatePicker } from 'antd';
-import { UserOutlined, LockOutlined } from '@ant-design/icons';
 import city from '../img/city.png'
 
 const { Title, Paragraph } = Typography;
@@ -8,9 +10,10 @@ const {RangePicker} = DatePicker;
 
 const Main = ()=>{
     const [form] = Form.useForm();
-    const [peopleCount,setPeopleCount]= useState(2);
+    const [peopleCount,setPeopleCount]= useState(1);
     const [numberCount,setNumberCount]= useState(1);
-    const [childrenCount,setChildrenCount]= useState(1)
+    const [childrenCount,setChildrenCount]= useState(0);
+    const [isVisible,setVisible]= useState(false);
     const [, forceUpdate] = useState();
 
     // To disable submit button at the beginning.
@@ -21,21 +24,25 @@ const Main = ()=>{
     const onFinish = values => {
         console.log('Finish:', values);
     };
+    const disabledDate = (current) => {
+        // Can not select days before today and today
+        return current <= (moment().endOf('day'))+1;
+    };
     return(
         <div className='main'>
             <Title level={3} style={{textAlign:'center'}}>Найдите спецпредложения для отелей, домов и других вариантов</Title>
             <Paragraph style={{textAlign:'center'}}>От уютных загородных домов до стильных городских квартир</Paragraph>
-            <Form form={form} name="horizontal_login" layout="inline" onFinish={onFinish} style={{margin:'7% 14%'}}>
+            <Form form={form} name="horizontal_login" layout="inline" onFinish={onFinish} style={{margin:'7% 13%'}}>
             <Form.Item name="city"
                 rules={[{ required: true, message: 'Please input city!' }]}
             >
                 <Input prefix={<img src={city}/>} placeholder="Город" />
             </Form.Item>
             <Form.Item>
-                <RangePicker style={{height:'34px'}}/>
+                <RangePicker disabledDate={disabledDate} locale={locale} style={{height:'34px'}}/>
             </Form.Item>
             <Form.Item>
-                <Button style={{height:'34px'}}>
+                <Button style={{height:'34px',width:'270px'}} onClick={()=>isVisible?setVisible(false):setVisible(true)}>
                     {peopleCount>1?`${peopleCount} взрослых`:'1 взрослый'} &#183; {childrenCount==0?'без детей':childrenCount==1?'1 ребёнок':`${childrenCount} детей`} &#183; {numberCount==1?'1 номер':(numberCount>1&&numberCount<5)?`${numberCount} номера`: `${numberCount} номеров`}
                 </Button>
             </Form.Item>
@@ -54,6 +61,22 @@ const Main = ()=>{
                 </Button>
                 )}
             </Form.Item>
+            {
+                isVisible?
+                <div class='modal'>
+                    <div className='modal-item'><span className='modal-label'>Взрослых: </span><Button className='modal-button' onClick={()=>peopleCount==1?1:setPeopleCount(peopleCount-1)}>-</Button>
+                     <span className='modal-value'>{peopleCount}</span>
+                    <Button className='modal-button' onClick={()=>peopleCount<20?setPeopleCount(peopleCount+1):20}>+</Button></div>
+                    <div className='modal-item'><span className='modal-label'>Номеров: </span><Button className='modal-button' onClick={()=>numberCount==1?1:setNumberCount(numberCount-1)}>-</Button>
+                    <span className='modal-value'>{numberCount}</span>
+                    <Button className='modal-button' onClick={()=>numberCount<10?setNumberCount(numberCount+1):10}>+</Button></div>
+                    <div className='modal-item'><span className='modal-label'>Детей: </span><Button className='modal-button' onClick={()=>childrenCount==0?0:setChildrenCount(childrenCount-1)}>-</Button>
+                    <span className='modal-value'>{childrenCount}</span>
+                    <Button className='modal-button' onClick={()=>childrenCount<10?setChildrenCount(childrenCount+1):10}>+</Button></div>
+                </div>
+                :''
+            }
+            
             </Form>
         </div>
     )
