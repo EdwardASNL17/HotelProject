a07c5b56ed3ad29d96399ef0d8c457adc9c1f9bc
EdwardASNL17 <edwardasnl@mail.ru>
2021-01-12 18:22:34 +0300

    Добавлена БД, отредактирован проект, подключена БД и добавлены данные

---
+++ FindHotel/FindHotel/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using FindHotel.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FindHotel.Controllers
+{
+    public class HomeController : Controller
+    {
+        FHContext db;
+        public HomeController(FHContext context)
+        {
+            db = context;
+        }
+        public IActionResult Index()
+        {
+            return View(db.Hotels.ToList());
+        }
+        
+
+    }
+}
---
+++ FindHotel/FindHotel/Models/FHContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace FindHotel.Models
+{
+    public class FHContext:DbContext
+    {
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public FHContext(DbContextOptions<FHContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        
+    }
+}
