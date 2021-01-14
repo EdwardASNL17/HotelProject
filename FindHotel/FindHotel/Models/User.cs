ef04bd8d69b5f31dab0db02cabfe8667f847ca14
EdwardASNL17 <edwardasnl@mail.ru>
2021-01-14 21:14:53 +0300

    Добавлена регистрация и авторизация

--- FindHotel/FindHotel/Models/User.cs
+++ FindHotel/FindHotel/Models/User.cs
@@ -1,20 +1,17 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace FindHotel.Models
 {
-    public class User
+    public class User:IdentityUser
     {
-       public int Id { get; set; }
-       public string Username { get; set; }
-       public string Password { get; set; }
-       public string Email { get; set; }
-       public bool IsAdmin { get; set; }
+       
        public string Name { get; set; }
        public string Surname { get; set; }
        public DateTime BirthDate { get; set; }
-       public DateTime RegDate { get; set; }
+       
     }
 }
---
+++ FindHotel/FindHotel/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FindHotel.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
+        
+        [Required]
+        [Display(Name = "Фамилия")]
+        public string Surname { get; set; }
+        
+        [Required]
+        [Display(Name = "Имя")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Дата Рождения")]
+        public DateTime BirthDate { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Подтвердить пароль")]
+        public string PasswordConfirm { get; set; }
+    }
+}
