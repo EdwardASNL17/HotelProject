743f7dc32e6989a7bc516d87d5d341409727c302
yom <jaxad@ya.ru>
2021-01-17 21:44:04 +0300

    Джейсон логин

--- FindHotel/FindHotel/Controllers/AccountController.cs
+++ FindHotel/FindHotel/Controllers/AccountController.cs
@@ -45,8 +45,7 @@ namespace FindHotel.Controllers
                
 
                 // Do something
-
-                    
+       
                     RegisterViewModel model = JsonSerializer.Deserialize<RegisterViewModel>(body.Result);
                     User user = new User { Email = model.Email, UserName = model.UserName, Surname = model.Surname, Name = model.Name, BirthDate = model.BirthDate};
  
@@ -71,46 +70,54 @@ namespace FindHotel.Controllers
                         }
                         return Json("Error");
                     }
-                
-                        
                     
             }
-
-
-
         }
-        [HttpGet]
+       /* [HttpGet]
         public IActionResult Login(string returnUrl = null)
         {
             return View(new LoginViewModel { ReturnUrl = returnUrl });
         }
-
+       */
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Login(LoginViewModel model)
+        public async Task<JsonResult> Login()
         {
-            if (ModelState.IsValid)
+            using (var reader = new StreamReader(Request.Body))
             {
-                var result =
-                    await _signInManager.PasswordSignInAsync(model.UserName, model.Password, model.RememberMe, false);
-                if (result.Succeeded)
-                {
-                    // проверяем, принадлежит ли URL приложению
-                    if (!string.IsNullOrEmpty(model.ReturnUrl) && Url.IsLocalUrl(model.ReturnUrl))
+                var body = reader.ReadToEndAsync();
+
+                LoginViewModel model = JsonSerializer.Deserialize<LoginViewModel>(body.Result);
+                //if (ModelState.IsValid)
+                //{
+                    var result =
+                        await _signInManager.PasswordSignInAsync(model.UserName, model.Password, model.RememberMe, false);
+                    if (result.Succeeded)
                     {
-                        return Redirect(model.ReturnUrl);
+                        return Json(model.UserName);
+                        // проверяем, принадлежит ли URL приложению
+                        /*  if (!string.IsNullOrEmpty(model.ReturnUrl) && Url.IsLocalUrl(model.ReturnUrl))
+                          {
+                              return Redirect(model.ReturnUrl);
+                          }
+                          else
+                          {
+                              return RedirectToAction("Cabinet", "Account");
+                          }
+                        */
                     }
                     else
-                    {
-                        return RedirectToAction("Cabinet", "Account");
+                    {   /*
+                        foreach (var error in result.Errors)
+                        {
+                            ModelState.AddModelError(string.Empty, error.Description);
+                        }
+                        */
+                        return Json("Error");
                     }
-                }
-                else
-                {
-                    ModelState.AddModelError("", "Неправильный логин и (или) пароль");
-                }
+                //}
+                //return View(model);
             }
-            return View(model);
         }
 
         [HttpPost]
